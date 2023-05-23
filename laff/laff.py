5633b97581ef63b57b8050ec955651d4f2a1d375
Adam Hennessy <ah724@leicester.ac.uk>
2023-05-23 13:22:16 +0100

    poetry rewrite (2)

--- laff/laff.py
+++ laff/laff.py
@@ -65,3 +65,21 @@ def findFlares(data):
             flare_end.append(end)
 
     return flare_start, flare_peak, flare_end
+
+def fitContinuum(data, flare_indices):
+
+    def _fitModel(data, model, par):
+
+        return
+
+    data_continuum = data
+
+    # Remove flare data.
+    flare_indices = sorted(flare_indices, key=lambda x: x[1], reverse=True)
+    for start, end in flare_indices:
+        del data_continuum[start:end]
+    
+    # Guess initial parameters.
+
+    return
+
