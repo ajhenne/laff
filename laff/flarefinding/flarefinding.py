4bc4ba4cead9a6cabf3bb8a65bf15acaa8c97540
Adam Hennessy <ahenneuk@live.com>
2025-02-05 10:09:15 +0000

    update algorithm choosing handling

--- laff/flarefinding/flarefinding.py
+++ laff/flarefinding/flarefinding.py
@@ -6,23 +6,26 @@ logger = logging.getLogger('laff')
 
 def flare_finding(data, algorithm):
     """Get the intended algorithm and find flares."""
-
-    if algorithm in ('default', ''):
-        algorithm = 'sequential'
-
-    #####
     
-    if algorithm == 'sequential':
+    if algorithm in ('default', 'sequential', ''):
         from .algorithms import sequential
-        return sequential(data)
+        flares = sequential(data)
     
     elif algorithm == 'sequential_smooth':
         from .algorithms import sequential
-        return sequential(data, smooth=True)
+        flares = sequential(data, smooth=True)
     
     elif algorithm == 'test':
         from .algorithms import apply_filter
-        return apply_filter(data)
+        flares = apply_filter(data)
+    
+    else:
+        raise ValueError("invalid algorithm used")
+    
+    if flares is not False:
+        logger.info("Found %s flare(s).", {len(flares)})
+    else:
+        logger.info('No flares found')
 
-    return
+    return flares
 
