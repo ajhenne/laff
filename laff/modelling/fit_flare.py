a74465dccf6e69ba6c4f9df3acf504af95789a40
Adam Hennessy <ahenneuk@live.com>
2025-05-20 15:22:15 +0100

    cleanup and neat packaging of flares

--- laff/modelling/fit_flare.py
+++ laff/modelling/fit_flare.py
@@ -97,189 +97,33 @@ def flare_fitter(data, continuum, flares, model='fred'):
         flareErrs.append(list(param_errors))
 
     logger.info("Flare fitting complete for all flares.")
-    return flareFits, flareErrs
+    return flareFits, flareStats, flareErrs
 
+#################################################################################
+### NEAT PACKAGING
+#################################################################################
 
-def improved_end_time(data, flare_indices, flare_par, continuum_par):
-    """
-    Find the end of the flares based on flare converging to continuum.
-    
-    Calculate the value of the fitted continuum model to the flare+continuum
-    model for a series of values until the models converge. Since the FRED
-    model is asymptotic to 0, we apply 'factor' to the flare+continuum, an
-    small effective downshift.
-    
-    Parameters:
-    data (pd.DataFrame): GRB lightcurve data.
-    flare_indices (List[int]): a nested list of indices corresponding to flare
-                               start, stop and end times.
-    flare_par (List[float]): a list of the flare model parameters.
-    continuum_par (List[floart]): a list of the continuum model parameters.
+def package_flares(data, fits, stats, errs, indices, count_ratio=1.0):
 
-    Returns:
-    end_index (int): the newly found index for the end of the flare.
-    end_time (float): the newly found time for the end of the flare.
-    """
+    flaresDict = []
 
-    logger.debug(f"Running improved_end_time for flare: {flare_indices}.")
-    peak, end = flare_indices[1:]
-    factor = 0.99 # seems to be the most consistent value.
+    for idx, fit, stat, err in zip(indices, fits, stats, errs):
 
-    if end == len(data.time) - 1:
-        logger.debug("End of flare already at end of data. Returning original inputs.")
-        return end, data['time'].iloc[end]
+        # tmax, rise, decay, sharp, ampl
+        # 0     1     2      3      4
+        fitted_flare = fred_flare(fit, data['time'])
 
-    # Calculate value for each model at current flare end.
-    current_continuum = broken_powerlaw([data['time'].iloc[end]], continuum_par)[0]
-    current_flare = fred_flare([data['time'].iloc[end]], flare_par)[0]
-    current_flare += (current_continuum * factor)
+        start_time = fit[0] - (fit[1] * (-np.log(0.001)) **(1/fit[3]))
+        peak_time  = fit[0]
+        end_time = fit[0] + (fit[2] * (-np.log(0.001)) **(1/fit[3]))
+        times = [start_time, peak_time, end_time]
 
-    # Loop until the flare+continuum model drops below the continuum.
-    extend = 0
-    while current_flare > current_continuum and end + extend < len(data.time) -1:
-        extend += 1
-        logger.debug(f"Conditions not yet met -- extending to {extend}")
-        current_continuum = broken_powerlaw([data['time'].iloc[end+extend]], continuum_par)[0]
-        current_flare = fred_flare([data['time'].iloc[end+extend]], flare_par)[0]
-        current_flare += (current_continuum * factor)
-    end_index = end + extend
-    logger.debug(f"Conditions met! Calculating intercept.")
+        fluence_rise  = calculate_fluence(fred_flare, fit, start_time, peak_time, count_ratio=count_ratio)
+        fluence_decay = calculate_fluence(fred_flare, fit, peak_time, end_time, count_ratio=count_ratio)
+        fluences = [fluence_rise, fluence_decay, fluence_rise + fluence_decay]
+
+        peak_flux = max(fitted_flare)
         
-    # Calculate the intercept time.
-    search_time = np.logspace(
-        np.log10(data['time'].iloc[peak+1]), 
-        np.log10(data['time'].iloc[end+extend+1]),
-        num = 1000)
-    
-    continuum_model = broken_powerlaw(search_time, continuum_par)
-    flare_model = fred_flare(search_time, flare_par) + (continuum_model * factor)
-    end_time, _ = intersection(search_time, continuum_model, search_time, flare_model)
+        flaresDict.append({'times': times, 'indices': idx, 'params': fit, 'stats': stat, 'errors': err, 'fluence': fluences, 'peak_flux': peak_flux})
 
-    # If no intersect found:
-    if len(end_time) == 0:
-        # print("NO TIME FOUND")
-        end_time = data['time'].iloc[end]
-
-    elif len(end_time) > 1:
-        logger.warning("Multiple intercepts found for this burst - the flare \
-                       may be incorrect or modelled badly.")
-        end_time = end_time[-1]
-        # print("WEVE ENTERED GREATER THAN ONE")
-        # import matplotlib.pyplot as plt
-        # plt.plot(search_time, continuum_model, color='tab:orange')
-        # plt.plot(search_time, flare_model, color='r')
-        # plt.scatter(data.time, data.flux, color='b', marker='.')
-        # plt.loglog()
-        # plt.show()
-        # raise ValueError("It appears two intercepts were found!")
-        ## This can occur when the found flare isn't really a flare. Noisy data
-        ## etc will cause the flare to be modelled wrong, and so multiple
-        ## intersections happen. I can't think of a scenario otherwise? There
-        ## should only ever be 2, so just select the later one in this case.
-        ## Maybe I can remove this check with better flare finding?
-    else:
-        end_time = end_time[0]
-        # print("WEVE NOT NOT NOT")
-    logger.debug(f"Found new end information: index {end_index}, end_time {end_time}.")
-    # print(end_time)
-    return end_index, end_time
-
-
-#################################################################################
-### FITTING METHODS
-#################################################################################
-
-def odr_fitter(data, inputpar, model_wrapper):
-    data = RealData(data.time, data.flux, data.time_perr, data.flux_perr)  
-    model = Model(model_wrapper)
-    odr = ODR(data, model, beta0=inputpar)
-
-    odr.set_job(fit_type=0)
-    output = odr.run()
-
-    if output.info != 1:
-        i = 1
-        while output.info != 1 and i < 100:
-            output = odr.restart()
-            i += 1
-
-    return output.beta, output.sd_beta
-
-def fit_flare_mcmc(data, init_param, init_err):
-
-    ndim = len(init_param)
-    nwalkers = 30
-    nsteps = 300
-
-    p0 = np.zeros((nwalkers, ndim))
-
-    guess_tstart = init_param[0]
-    std_tstart = init_param[0]
-    p0[:, 0] = guess_tstart + std_tstart * np.random.randn(nwalkers)
-
-    guess_rise = init_param[1]
-    std_rise = init_param[1]
-    p0[:, 1] = guess_rise + std_rise * np.random.randn(nwalkers)
-
-    guess_decay = init_param[2]
-    std_decay = init_param[2]
-    p0[:, 2] = guess_decay + std_decay * np.random.randn(nwalkers)
-
-    guess_amplitude = init_param[3]
-    std_ampltiude = init_param[3]
-    p0[:, 3] = guess_amplitude + std_ampltiude * np.random.randn(nwalkers)
-
-    logger.debug("Running flare MCMC...")
-
-    sampler = emcee.EnsembleSampler(nwalkers, ndim, fl_log_posterior, \
-        args=(data.time, data.flux, data.time_perr, data.flux_perr))
-    sampler.run_mcmc(p0, nsteps)
-
-    burnin = 50
-
-    samples = sampler.chain[:, burnin:, :].reshape(-1, ndim)
-
-    fitted_par = list(map(lambda v: np.median(v), samples.T))
-    fitted_err = list(map(lambda v: np.std(v), samples.T))
-
-    logger.debug("MCMC run completed.")
-
-    return fitted_par, fitted_err
-
-def fl_log_likelihood(params, x, y, x_err, y_err):
-    model = fred_flare(x, params)
-    chisq = np.sum(( (y-model)**2) / ((y_err)**2)) 
-    log_likelihood = -0.5 * np.sum(chisq + np.log(2 * np.pi * y_err**2))
-    return log_likelihood   
-
-def fl_log_prior(params, TIME_END):
-
-    tau = params[0]
-    rise = params[1]
-    decay = params[2]
-    amplitude = params[3]
-
-    if not (tau > 0) and (tau < TIME_END):
-        return -np.inf
-
-    if rise < 0:
-        return -np.inf
-    if rise > TIME_END:
-        return -np.inf
-
-    if decay < 0:
-        return -np.inf
-    if decay > TIME_END:
-        return -np.inf
-
-    if amplitude < 0:
-        return -np.inf
-
-    return 0.0
-
-def fl_log_posterior(params, x, y, x_err, y_err):
-    lp = fl_log_prior(params, x.iloc[-1])
-    ll = fl_log_likelihood(params, x, y, x_err, y_err)
-    if not np.isfinite(lp):
-        return -np.inf
-    return lp + ll
+    return flaresDict
