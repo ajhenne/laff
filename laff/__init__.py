7d670fdd76fd537d6672d8e0da4971db742e3811
Adam Hennessy <ahenneuk@live.com>
2025-05-21 17:00:44 +0100

    remove extr aplotting function

--- laff/__init__.py
+++ laff/__init__.py
@@ -1,4 +1,9 @@
-from .laff import fitGRB, fitAfterglow, plotGRB, set_logging_level, findFlares, fitFlares
+## Main functions.
+from .laff import fitGRB, findFlares, fitFlares, fitAfterglow
+from .laff import plotGRB, printGRB
 from .lightcurve_import import lcimport
-from .utility import STAT_NAMES_CONTINUUM, PAR_NAMES_FLARE, PAR_NAMES_CONTINUUM
 from .fit_bat import fitPrompt, plotPrompt, fred_flares
+
+## Auxilliary.
+from .laff import set_logging_level
+from .utility import STAT_NAMES_CONTINUUM, PAR_NAMES_FLARE, PAR_NAMES_CONTINUUM
