adac21012a26f26c5005cdf36393a993b2944e9e
Adam Hennessy <ahenneuk@live.com>
2025-07-10 15:50:38 +0100

    tighten bat fitting

--- laff/fit_bat.py
+++ laff/fit_bat.py
@@ -9,6 +9,8 @@ from .utility import calculate_fit_statistics, calculate_fluence, calculate_par_
 
 def fitPrompt(data):
 
+    data = data[data['flux'] != 0.0].reset_index(drop=True)
+
     data, flares = filter_data(data)
 
     # continuum = find_continuum(data, flares)
@@ -24,6 +26,15 @@ def fitPrompt(data):
 def filter_data(data):
 
     data['savgol'] = savgol_filter(data['flux'], window_length=31, polyorder=3)
+    
+    data['negative_noise'] = data['flux'].copy()
+    data['negative_noise'] = data['negative_noise'].apply(lambda x: min(x, 0))
+    data['negative_noise'] = data['negative_noise'].rolling(window=50, min_periods=1).std() * 2
+    data.loc[data['time'] < data['time'].iloc[0] * 0.95, 'negative_noise'] *= 2
+    data.loc[data['time'] > data['time'].iloc[-1] * 0.95, 'negative_noise'] *= 2
+    data.loc[0, 'negative_noise'] = data['negative_noise'].iloc[1]
+
+
 
     # Filter out negatives.
     filtered_data = data['savgol'].copy()
@@ -130,8 +141,8 @@ def fit_flares(data, flare_indices):
             
                 # Parameter bounds
                 t_peak_bound    = [data['time'].iloc[dev_start], data['time'].iloc[dev_end]]
-                rise_bound      = [rise/10, t_end-t_start]
-                decay_bound     = [decay/10, t_end-t_start]
+                rise_bound      = [rise/10, rise/3]
+                decay_bound     = [decay/10, decay/3]
                 sharp_bound     = [1.0, 5.0]
                 amplitude_bound = [0, 2 * data['flux'].max()]
 
@@ -140,7 +151,6 @@ def fit_flares(data, flare_indices):
             def all_constraints():
                 pass
 
-            print('yes')
             fitted_flare = fmin_slsqp(sum_residuals, input_par, bounds=bounds, args=(flare_data.time, flare_data.untouched_flux, flare_data.flux_perr), iter=200, iprint=0)
 
             fitted_stats = calculate_fit_statistics(flare_data, fred_flare, fitted_flare)
@@ -163,16 +173,23 @@ def fit_flares(data, flare_indices):
                     flare_count -= 1
                     try_another = False
 
-        flare_data['flux'] -= fred_flare(fitted_flare, flare_data['time'])
-        flare_data['flux'] = flare_data['flux'].apply(lambda x: max(x, 0))
-
         for i in range(0, len(fitted_flare), 5):
+
+            if fitted_flare[i+4] < 1 * np.average(data['negative_noise'].iloc[dev_start:dev_end]):
+                # print('cooked that flare', fitted_flare[i])
+                continue
+
+            # if data['time'].iloc[-1]
+
+            flare_data['flux'] -= fred_flare(fitted_flare[i:i+5], flare_data['time'])
+            flare_data['flux'] = flare_data['flux'].apply(lambda x: max(x, 0))
 
             fluence_rise = calculate_fluence(fred_flare, fitted_flare[i:i+5], t_start, fitted_flare[i], 1)
             fluence_decay = calculate_fluence(fred_flare, fitted_flare[i:i+5], fitted_flare[i], t_end, 1)  
             fluence_total = fluence_rise + fluence_decay
             
             flares.append({'indices': (dev_start, dev_end), 'parameters': fitted_flare[i:i+5], 'fluence': [fluence_rise, fluence_decay, fluence_total], 'fit_statistics': fitted_stats})
+
 
     return flares
 
@@ -209,7 +226,7 @@ def plotPrompt(prompt_fit, **kwargs):
     else:
         fig, ax1 = plt.subplots(figsize=(10, 6))
 
-    
+    constant_range = np.linspace(data['time'].iloc[0], data['time'].iloc[-1], num=len(data['time']) * 10)
 
     # Plot title.
     if (grb_name := kwargs.get('grb_name')):
@@ -234,7 +251,7 @@ def plotPrompt(prompt_fit, **kwargs):
         ax2.plot(data['time'], data['savgol_residuals'], linewidth=0.5, color='tab:green')
 
 
-    total_model = [0.0] * len(data['flux'])
+    total_model = [0.0] * constant_range
 
     for flare in prompt_fit['flares']:
 
@@ -245,19 +262,23 @@ def plotPrompt(prompt_fit, **kwargs):
             if residuals:
                 ax2.axvspan(data['time'].iloc[srt], data['time'].iloc[end], color='b', alpha=0.2)
 
-        flare_model = fred_flare(flare['parameters'], data['time'])
+        flare_model = fred_flare(flare['parameters'], constant_range)
         total_model += flare_model
 
         if kwargs.get('flare_fit', True):
-            ax1.plot(data['time'], flare_model, color='#2274A5', linewidth=1)
+            ax1.plot(constant_range, flare_model, color='#2274A5', linewidth=1)
             if residuals:
-                ax2.plot(data['time'], flare_model, color='cyan', linewidth=1)
+                ax2.plot(constant_range, flare_model, color='cyan', linewidth=1)
 
     if kwargs.get('total_fit', True):
-        ax1.plot(data['time'], total_model, color='tab:orange', linewidth=2)
+        ax1.plot(constant_range, total_model, color='tab:orange', linewidth=2)
 
     plt.xlabel('Time since trigger (s)', fontsize=16)
     plt.ylabel('Count rate (counts/s)', fontsize=16)
+
+    # plt.axvline(data['time'].iloc[0] * 0.95, color='r')
+    # plt.axvline(data['time'].iloc[-1] * 0.95, color='r')
+    # plt.plot(data['time'], data['negative_noise'], color='m')
 
     # plt.xlim(-50, 200)
     # plt.ylim(-0.2, 0.4)
